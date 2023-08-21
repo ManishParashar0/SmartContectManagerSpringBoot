2e0523454798af438e108e22fef3274ce0386ac4
Manish parashar <121606120+ManishParashar0@users.noreply.github.com>
2023-08-21 16:50:30 +0530

    added frontend

--- src/main/java/com/manish/Smartmanager/Controller/Homecontroller.java
+++ src/main/java/com/manish/Smartmanager/Controller/Homecontroller.java
@@ -2,10 +2,8 @@ package com.manish.Smartmanager.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.manish.Smartmanager.Entity.User;
 import com.manish.Smartmanager.repo.UserRepo;
 
 @Controller
@@ -13,14 +11,28 @@ public class Homecontroller {
 	@Autowired
 	private UserRepo repo;
 
-	@GetMapping("/test")
-	@ResponseBody
+	/*
+	 * @GetMapping("/")
+	 * 
+	 * @ResponseBody
+	 */
+	@RequestMapping("/")
 	public String test() {
-		User user = new User();
-		user.setName("manish");
-		user.setEmail("manisns@gmail.com");
-		repo.save(user);
-		return "test";
+		/*
+		 * User user = new User(); user.setName("manish");
+		 * user.setEmail("manisns@gmail.com"); repo.save(user);
+		 */
+		return "NewFile";
+
+	}
+
+	@RequestMapping("/Register")
+	public String test1() {
+		/*
+		 * User user = new User(); user.setName("manish");
+		 * user.setEmail("manisns@gmail.com"); repo.save(user);
+		 */
+		return "Register";
 
 	}
 }
